a69cadf3a150af057e68d7e8ae0dc48fb51c3e11
kangsei <yhs7409@naver.com>
2022-02-10 19:28:53 +0900

    <label>:<title> # 제목의 길이는 최대 40글자까지 한글로 간단 명료하게 작성 # 제목을 작성하고 반드시 빈 줄 한 줄을 만들어야 함 # 제목에 .(마침표) 금지 # <label> 리스트 # feature : 새로운 기능 # bug : 버그 수정 # update : 비즈니스 로직 변경 # docs : 문서 (문서 추가, 수정, 삭제) # test : 테스트 # etc : 기타 변경사항 <description> # 내용의 길이는 한 줄당 60글자 내외에서 줄 바꿈. 한글로 간단 명료하게 작성 # 어떻게 보다는 무엇을, 왜 변경했는지를 작성할 것 (필수) <issue-number> # 연관된 이슈 첨부, 여러 개 추가 가능
    출처: https://jeong-pro.tistory.com/207 [기본기를 쌓는 정아마추어 코딩블로그]

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log(540);
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add("hidden");
+    const username = loginInput.value;
+    greeting.innerText = "Hello " + username;
+    greeting.classList.remove("hidden")l
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
